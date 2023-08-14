224d0e382e2a1b5dbdca0876d8a8524fb5c8a1b8
wtgtybhertgeghgtwtg <wtgtybhertgeghgtwtg@gmail.com>
2023-08-13 20:05:38 -0700

    chore: remove unused files

--- scripts/jest/config.dist.js
+++
@@ -1,6 +0,0 @@
-const sourceConfig = require('./config.source');
-
-module.exports = {
-  ...sourceConfig,
-  moduleNameMapper: {'../src': '<rootDir>/dist/index.cjs'},
-};
--- scripts/jest/config.source.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-  collectCoverageFrom: ['src/**/*.js', '!src/{index,types}.js'],
-  rootDir: process.cwd(),
-  setupFiles: ['@babel/polyfill'],
-  testEnvironment: 'node',
-  testMatch: ['**/*.test.js'],
-};
